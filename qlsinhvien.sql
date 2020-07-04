cd3690548b296999cedd174208be8e00b6cfb8e9
HungCao242 <63040210+HungCao242@users.noreply.github.com>
2020-07-04 10:05:33 +0700

    Add files via upload
    database

---
+++ qlsinhvien.sql
@@ -0,0 +1,151 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th7 04, 2020 lúc 03:50 AM
+-- Phiên bản máy phục vụ: 10.4.11-MariaDB
+-- Phiên bản PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `qlsinhvien`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `ketqua`
+--
+
+CREATE TABLE `ketqua` (
+  `MaKQ` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `MaMH` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `MaSV` int(10) NOT NULL,
+  `Diem` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `khoa`
+--
+
+CREATE TABLE `khoa` (
+  `MaKhoa` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `TenKhoa` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `NamThanhLap` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `lop`
+--
+
+CREATE TABLE `lop` (
+  `MaLop` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `TenLop` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `MaKhoa` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `monhoc`
+--
+
+CREATE TABLE `monhoc` (
+  `MaMH` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `TenMon` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `SoTC` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `sinhvien`
+--
+
+CREATE TABLE `sinhvien` (
+  `MaSV` int(10) NOT NULL,
+  `HoTen` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `NgaySinh` date NOT NULL,
+  `Email` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `MaLop` varchar(10) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `ketqua`
+--
+ALTER TABLE `ketqua`
+  ADD PRIMARY KEY (`MaKQ`),
+  ADD KEY `MaMH` (`MaMH`),
+  ADD KEY `MaSV` (`MaSV`);
+
+--
+-- Chỉ mục cho bảng `khoa`
+--
+ALTER TABLE `khoa`
+  ADD PRIMARY KEY (`MaKhoa`);
+
+--
+-- Chỉ mục cho bảng `lop`
+--
+ALTER TABLE `lop`
+  ADD PRIMARY KEY (`MaLop`),
+  ADD KEY `MaKhoa` (`MaKhoa`);
+
+--
+-- Chỉ mục cho bảng `monhoc`
+--
+ALTER TABLE `monhoc`
+  ADD PRIMARY KEY (`MaMH`);
+
+--
+-- Chỉ mục cho bảng `sinhvien`
+--
+ALTER TABLE `sinhvien`
+  ADD PRIMARY KEY (`MaSV`),
+  ADD KEY `MaLop` (`MaLop`);
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `ketqua`
+--
+ALTER TABLE `ketqua`
+  ADD CONSTRAINT `ketqua_ibfk_1` FOREIGN KEY (`MaMH`) REFERENCES `monhoc` (`MaMH`),
+  ADD CONSTRAINT `ketqua_ibfk_2` FOREIGN KEY (`MaSV`) REFERENCES `sinhvien` (`MaSV`);
+
+--
+-- Các ràng buộc cho bảng `lop`
+--
+ALTER TABLE `lop`
+  ADD CONSTRAINT `lop_ibfk_1` FOREIGN KEY (`MaKhoa`) REFERENCES `khoa` (`MaKhoa`);
+
+--
+-- Các ràng buộc cho bảng `sinhvien`
+--
+ALTER TABLE `sinhvien`
+  ADD CONSTRAINT `sinhvien_ibfk_1` FOREIGN KEY (`MaLop`) REFERENCES `lop` (`MaLop`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
