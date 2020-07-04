8038b624011236beaea531812423e883b96b96b2
Ngo Hoang Minh <powpow827@gmail.com>
2020-07-04 11:49:39 +0700

    mennu

---
+++ menu.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="menu.css"/>
+    </head>
+    <body>
+        <div class="menu">
+            <ul>
+                <li><a href="xemthongtinsv.php" target="noidung">Xem thông tin Sinh Viên</a></li>
+                <li><a href="timkiem.php" target="noidung">Tìm kiếm</a></li>
+                <li><a href="capnhatkhoa.php" target="noidung">Cập nhật, xóa</a></li>
+                <li><a href="suadiem.php" target="noidung">Nhập, Xem Điểm Theo Môn</a></li>
+                <li><a href="xemdiem.php" target="noidung">Xem Điểm Sinh Viên Theo Lớp</a></li>
+            </ul>
+        </div>
+    </body>
+</html>
